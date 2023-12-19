0daec2636ad86157aff112dd7e0a5784fb26db49
KevinFerry1 <kferry@montgomerycollege.edu>
2023-12-18 22:09:11 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+for player in playerList:
+    #if else conditionals to make sure you are meeting criteria in squad of 100M budget, 15 players, 2 GK, 5 def, 5 mid, and 3 forwards. Also no more than 3 players from each team.
+    
